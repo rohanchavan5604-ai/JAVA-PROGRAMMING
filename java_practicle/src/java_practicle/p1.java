6677bcf792881b4f7c0a192391fd8269ec45f3db
Rohan Chavan <rohanchavan5604@gmail.com>
2026-05-17 16:03:49 +0530

    first commit through Eclipse IDE

---
+++ java_practicle/src/java_practicle/p1.java
@@ -0,0 +1,11 @@
+package java_practicle;
+
+public class p1 {
+
+	public static void main(String[]sp)
+	{
+		for(int i = 1, j=1; i<=10; i++) {
+			System.out.println(j);
+		}
+ 	}
+}
---
+++ java_practicle/src/java_practicle/rc.java
@@ -0,0 +1,10 @@
+package java_practicle;
+
+public class rc {
+
+	public static void main(String[] args) {
+	 
+
+	}
+
+}
