d1fc5cf6fda3974ee7e57368a165f2404885df49
Rohan Chavan <rohanchavan5604@gmail.com>
2026-05-18 16:43:04 +0530

    Anather one for ARRAY

---
+++ java_practicle/src/com/Rohan/arr.java
@@ -0,0 +1,17 @@
+package com.Rohan;
+
+public class arr {
+
+	 public static void main(String[] args) {
+		int  nums[] = {10,220,340};
+		int sum = 0;{
+			
+			for(int i=0; i<nums.length;i++) {
+				sum = sum + nums[i];
+			}
+		}
+		System.out.println(sum);
+		
+	}
+
+}
