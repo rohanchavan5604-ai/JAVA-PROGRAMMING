8dd3693972d92675b5bb6d365adf32946b83330b
Rohan Chavan <rohanchavan5604@gmail.com>
2026-05-18 16:20:13 +0530

    core java's  ARRAY

---
+++ java_practicle/src/com/Rohan/array.java
@@ -0,0 +1,12 @@
+package com.Rohan;
+
+public class array {
+	public static void main(String[] args) {
+		
+		int[] nums = {10,20,30,40,50};
+		
+		System.out.println(nums[0]);
+		System.out.println(nums[4]);
+		System.out.println(nums[2]);
+	}
+}
