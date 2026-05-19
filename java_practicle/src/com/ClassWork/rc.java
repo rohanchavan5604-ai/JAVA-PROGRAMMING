45353cb9fd7d63385a15305530cd7ca4f342f76c
Rohan Chavan <rohanchavan5604@gmail.com>
2026-05-19 15:46:19 +0530

    Class Work

---
+++ java_practicle/src/com/ClassWork/rc.java
@@ -0,0 +1,42 @@
+package com.ClassWork;
+
+class rc {
+	public static void main(String[] args) {
+		
+//		 TASK
+		
+		// touristplace_name, city, distance, estmate_amm,trasport_type,whyYouLike;
+		
+		String place = "Ganpati Bappa Temple", city = "Tasgaon, Sangli",distance = "240 km",trasport_type = "train",WhyYouLike = "I feel Like Its Heaven";
+		int estimateAmm = 600;
+		
+		
+		System.out.println(place+" " + city+"\n"+ distance+" "+ trasport_type +" \n"+WhyYouLike);
+		
+		System.out.println(estimateAmm);
+		
+		
+		
+		
+		
+		
+		
+//		DEMO
+		 
+//		String name = "Rohan", add = "Bastawade, Sangli", bdate = "03-11-2005", qualification ="BCA";
+//		int age = 21;
+//		long MobNu = 9665885942L;
+//		float per = 87.56F;
+//		double fees = 35000.00;
+//		boolean IsRegister = true;
+//		char gender = 'M';
+//		short rno = 2241;
+//		byte  luckNuM = 45;
+//		
+//		
+//		System.out.println(name+ "  " + add +" "+ bdate+ " "+qualification);
+//		System.out.println(age +" "+ MobNu+" "+per+" "+ fees);
+//		System.out.println(IsRegister+" "+ gender+" "+rno+" "+ luckNuM);
+	}
+
+}
