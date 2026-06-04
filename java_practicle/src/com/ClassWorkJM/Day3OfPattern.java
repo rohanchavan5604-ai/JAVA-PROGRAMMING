01ad7f6c240becb6b5ac6f8dc45f2ba3ef73a831
Rohan Chavan <rohanchavan5604@gmail.com>
2026-06-04 22:08:12 +0530

    Pattern's

---
+++ java_practicle/src/com/ClassWorkJM/Day3OfPattern.java
@@ -0,0 +1,198 @@
+package com.ClassWorkJM;
+
+public class Day3OfPattern {
+	
+  public static void main(String[] args) {
+	  
+	  for(int i=5; i>=1;i--) {
+		  for(int j=5; j>=i; j--) {
+			  
+			  System.out.print(j);
+		  }
+		  for(int j=1;j<=i-1;j++) {
+			  System.out.print(i);
+		  }
+		  
+		  System.out.println();
+	  }
+	  
+	  
+	  
+		
+//		for (int i = 5; i >= 1; i--) {
+//		for (int j = 1; j <= 5; j++) {
+//			System.out.print(j);
+//		}
+//			for (int j = 1 ; j< i; j++) {
+//				System.out.print("5");
+//			}
+//
+//		System.out.println();
+//	}
+	  
+	  
+	  
+	  
+//		for (int i = 1; i <= 5; i++) {
+//		for (int j = 1; j <= 5 - i; j++) {
+//			System.out.print(i);
+//		}
+//		if (i >= 1 && i <= 5) {
+//			for (int s = 1; s <= i; s++) {
+//				System.out.print("5");
+//			}
+//		}
+//
+//		System.out.println();
+//	}
+ 
+		  
+		  
+		  
+//			for (int i = 1; i <= 5; i++) {
+//				for (int j = 1; j <= 5 - i; j++) {
+//					System.out.print("1");
+//				}
+//				if (i >= 1 && i <= 5) {
+//					for (int s = 1; s <= i; s++) {
+//						System.out.print(i);
+//					}
+//				}
+//
+//				System.out.println();
+//			}	
+		  
+		  
+		  
+		  
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=5; j++) {
+//				  
+//				  if(i==j) {
+//					 
+//					  System.out.print("1 ");		 
+//				  }else {
+//					  System.out.print("0 ");		
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	 
+		  
+		  
+		  
+		  
+		
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=5; j++) {
+//				  
+//				  if(i==j) {
+//					 
+//					  System.out.print(i+" ");		 
+//				  }else {
+//					  System.out.print("0 ");		
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	   
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=i; j++) {
+//				  
+//				  if(j%2!=0) {
+//					 
+//					  System.out.print("0 ");		 
+//				  }else {
+//					  System.out.print("1 ");		
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	   
+		  
+		  
+		  
+		  
+		  
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=i; j++) {
+//				  
+//				  if(j%2==0) {
+//					 
+//					  System.out.print("0 ");		 
+//				  }else {
+//					  System.out.print("1 ");		
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	   
+		  
+		  
+		  
+		  
+		  
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=5; j++) {
+//				  
+//				  if((i+j)%2==0) {
+//					 
+//					  System.out.print("0 ");		 
+//				  }else {
+//					  System.out.print("1 ");		
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	   
+		  
+		  
+		  
+		  
+		  
+		
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=5; j++) {
+//				  
+//				  if(j%2==0) {						//if(i%2!=0){   Anather way
+//					  System.out.print("0 ");		// 1
+//				  }else {
+//					  System.out.print("1 ");		// 0
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }	  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+//		  for(int i=1; i<=5; i++) {
+//			  for(int j=1; j<=5; j++) {
+//				  
+//				  if(i%2==0) {						//if(i%2!=0){   Anather way
+//					  System.out.print("0 ");		// 1
+//				  }else {
+//					  System.out.print("1 ");		// 0
+//				  }
+//				  
+//			  }
+//			  System.out.println();
+//		  }
+	
+
+		  
+		  
+	}
+
+}
