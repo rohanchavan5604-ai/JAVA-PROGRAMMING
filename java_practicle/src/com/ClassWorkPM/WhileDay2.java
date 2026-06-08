166446e9710c2c8f3e62bc679338d846154ce7fa
Rohan Chavan <rohanchavan5604@gmail.com>
2026-06-08 15:24:26 +0530

    While loop codes

---
+++ java_practicle/src/com/ClassWorkPM/WhileDay2.java
@@ -0,0 +1,80 @@
+package com.ClassWorkPM;
+
+public class WhileDay2 {
+
+	public static void main(String[] args) {
+		
+		
+//		int num = 110;
+//		int temp = num;
+//		int sum = 0;
+//	    while(num>0) {
+//	    	System.out.println(num%10);
+//	    	sum= sum + num%10;
+//	    	num = num / 10;
+//	    }
+//	    System.out.println("Sum of Digits: "+sum);
+//	    
+//	    if(temp % sum == 0) {
+//	    	System.out.println("Harshad Number");
+//	    }else {
+//	    	System.out.println("Not a harshad number");
+//	    }
+		
+		
+		
+		
+//		COUNT OF NUMBER USING WHILE 
+		
+//		int num = 7857675;
+//		int count = 0;
+//		int rem;
+//		while (num > 0) {
+//
+////			rem = num % 10;
+////		    System.out.println(rem);      // FOR COUNTER ONLY PRINT NO NUM SEPRATION	
+//		    count++;
+//			num = num / 10;
+//		}
+//		System.out.println("Count is: "+count);
+		
+		
+		
+		
+//	ONLY ODD NUMBER SEP PRINT
+		
+		
+//		int num = 123456;
+//		int sum = 0;
+//		int rem;
+//		while (num > 0) {
+//
+//			rem = num % 10;
+//			if (rem % 2 != 0) {
+//				System.out.println(rem);
+//			}
+//			num = num / 10;
+//		}
+		
+		
+		
+//		ONLY EVEN NUMBER SEP PRINT
+
+		
+//		int num = 123456;
+//		int sum = 0;
+//		int rem;
+//		while (num > 0) {
+//
+//			rem = num % 10;
+//			if (rem % 2 == 0) {
+//				System.out.println(rem);
+//			}
+//			num = num / 10;
+//		}
+		
+		
+
+	}
+
+}
