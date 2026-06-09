ad34c5742b79f2c0ce97a2eac24547224d7373bf
Rohan Chavan <rohanchavan5604@gmail.com>
2026-06-09 13:14:23 +0530

    Patter final Day codes

---
+++ java_practicle/src/com/ClassWorkJM/June_10.java
@@ -0,0 +1,11 @@
+package com.ClassWorkJM;
+
+public class June_10 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ java_practicle/src/com/ClassWorkJM/Pattern_Final_Day.java
@@ -0,0 +1,229 @@
+package com.ClassWorkJM;
+
+import java.util.Scanner;
+
+public class Pattern_Final_Day {
+
+	public static void main(String[] args) {
+	
+		
+		Scanner sc = new Scanner(System.in);
+	
+//		pattern + from taking input from user part 2
+		
+//        System.out.println("Enter Row and Column:");
+//        int row = sc.nextInt();
+//        int col = sc.nextInt();
+//
+//        for (int i = 1; i <= row; i++) {
+//            for (int j = 1; j <= col; j++) {
+//            	
+//            	if(j== (col/2+1))
+//            	{
+//            	 System.out.print(i);	
+//            	}else if (i == (row /2)+1 || j == (col /2)+1) {
+//                    System.out.print(j);
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+		
+		
+		
+		
+//		pattern + from taking input from user
+		
+		
+		
+//        System.out.println("Enter Row and Column:");
+//        int row = sc.nextInt();
+//        int col = sc.nextInt();
+//
+//        for (int i = 1; i <= row; i++) {
+//            for (int j = 1; j <= col; j++) {
+//
+//                if (i == (row + 1) / 2 || j == (col + 1) / 2) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+		
+		
+//		ADDITION SIGN PATTER USING STAR
+		
+		
+//		for(int i=1; i<=5; i++) {
+//			for(int j=1; j<=5; j++) {
+//				
+//				if(i==3 || j==3) {
+//					System.out.print("*");
+//				}
+//			 
+//				else {
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+//		PATTERN 83
+		
+//		for(int i=1; i<=5;i++) {
+//			for(int j=1; j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			for(int s=4; s>=i;s--) {
+//				System.out.print("  ");
+//			}
+//			for(int k=1; k<=i;k++) {
+//				System.out.print(k);
+//			}
+//			System.out.println();
+//		}
+
+		
+//		PATTERN 82
+		
+		
+//		for(int i=1; i<=5;i++) {
+//			for(int j=1; j<=i;j++) {
+//				System.out.print(i);
+//			}
+//			for(int s=4; s>=i;s--) {
+//				System.out.print("  ");
+//			}
+//			for(int k=1; k<=i;k++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+		
+	
+//		PATTERN 81
+		
+//		for(int i=1; i<=5;i++) {
+//			for(int j=1; j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			for(int s=4; s>=i;s--) {
+//				System.out.print("  ");
+//			}
+//			for(int k=i; k>=1;k--) {
+//				System.out.print(k);
+//			}
+//			System.out.println();
+//		}
+		
+	
+		
+//		BUTTRFLY  STAR  PATTERN
+		
+//		for (int i = 1; i <= 5; i++) {
+//			for (int star = 1; star <= i; star++) {
+//				System.out.print("*");
+//			}
+//			for (int s = 5; s > i; s--) {
+//				System.out.print("  ");
+//			}
+//			for(int star = 1; star <= i; star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		  for(int i = 4; i >= 1; i--) {
+//			for (int star = 1; star <= i; star++) {
+//			System.out.print("*");
+//			}
+//
+//			for (int s = 5; s > i; s--) {
+//			System.out.print("  ");
+//			}
+//
+//			for (int star = 1; star <= i; star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		
+//		DIFFICULT PATTERN
+		
+	
+//		for(int i=1; i<=5; i++) {
+//			for(int star=1; star<=i; star++) {
+//				System.out.print("*");
+//			}
+//			for(int s = 4; s>=i; s--) {
+//				System.out.print(" ");
+//			}
+//			for(int star=5; star>=i;star--) {
+//				System.out.print("*");
+//			}
+//			for(int s =1; s<i; s++) {
+//				System.out.print("  ");
+//			}
+//			for(int star=5; star>=i; star--) {
+//			System.out.print("*");
+//			}
+//			for(int s=4; s>=i; s--) {
+//				System.out.print(" ");
+//			}
+//			for(int star=1; star<=i; star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+ 	
+//		DIAMOND  HOLLO PATTERN
+		
+		
+// for(int i =1; i<=9; i++) {
+//	 
+//	 if(i>=1 && i<=5) 
+//	 {
+//		 for(int s= 4; s>=i; s--) {
+//			 System.out.print(" ");
+//		 }
+//		 for(int star=1; star<=i; star++) {
+//			 System.out.print("* ");
+//		 }
+//	 }
+//	 if(i>=6 && i<=9) 
+//	 {
+//		for(int s=1; s<=i-5; s++){
+//			System.out.print(" ");
+//		} 
+//		for(int star=10; star>i;star--) {
+//			System.out.print("* ");
+//		}
+//	 
+//	 }
+//	 System.out.println();
+//	 
+// }
+		
+		
+		
+	}
+	
+}
