0eab7477caf8578ffb84ada3078abbdc44ce9f5f
Rohan Chavan <rohanchavan5604@gmail.com>
2026-07-11 19:26:48 +0530

    Selection Sort in arr

---
+++ java_practicle/src/Array_PM/SelectionSortArray.java
@@ -0,0 +1,30 @@
+package Array_PM;
+
+public class SelectionSortArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = {3, 2, 1 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			int min = i;
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[min] > arr[j]) {
+
+					min = j;
+
+				}
+
+			}
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+			System.out.println(arr[i]);
+
+		}
+
+	}
+
+}
