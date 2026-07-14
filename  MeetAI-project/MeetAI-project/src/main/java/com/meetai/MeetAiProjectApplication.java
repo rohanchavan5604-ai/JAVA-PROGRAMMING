a31ededfc2c8720b71d9708af60543a82e49cb64
Rohan Chavan <rohanchavan5604@gmail.com>
2026-07-14 22:26:15 +0530

    Initial project setup with Spring Boot, Maven, MySQL, and JPA

---
+++  MeetAI-project/MeetAI-project/src/main/java/com/meetai/MeetAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.meetai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetAiProjectApplication.class, args);
+	}
+
+}
---
+++  MeetAI-project/MeetAI-project/src/test/java/com/meetai/MeetAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
