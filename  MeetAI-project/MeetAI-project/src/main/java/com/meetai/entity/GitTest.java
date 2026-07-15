7f1491a856719228ee0e5692e8be16e94224cd78
Rohan Chavan <rohanchavan5604@gmail.com>
2026-07-15 21:59:25 +0530

    Demo

---
+++  MeetAI-project/MeetAI-project/src/main/java/com/meetai/entity/GitTest.java
@@ -0,0 +1,16 @@
+package com.meetai.entity;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("================================");
+		System.out.println("Git Repository Test");
+		System.out.println("Name    : Rohan Arun Chavan");
+		System.out.println("Project : ADVANCE-JAVA");
+		System.out.println("Date    : 15-07-2026");
+		System.out.println("================================");
+
+	}
+
+}
