b254bbea01869456df980dce23a263074a054caf
Rohan Chavan <rohanchavan5604@gmail.com>
2026-07-29 10:52:40 +0530

    Queue Datastructure in Collection

---
+++ java_practicle/src/Collection_PM/Queue_DS.java
@@ -0,0 +1,28 @@
+package Collection_PM;
+
+import java.util.PriorityQueue;
+
+public class Queue_DS {
+
+	public static void main(String[] args) {
+
+		// no order
+		// homogeneous
+		// no null is allowed
+		// duplicate are allowed
+
+		PriorityQueue p = new PriorityQueue();
+		p.add(9);
+		p.add(8);
+		p.add(7);
+		p.add(6);
+		p.add(5);
+		p.add(4);
+		p.add(3);
+		p.add(2);
+		p.add(1);
+		System.out.println(p);
+
+	}
+
+}
--- java_practicle/src/Exeption_Handling_JM/Throws_ex.java
+++ java_practicle/src/Exeption_Handling_JM/Throws_ex.java
@@ -1,41 +1,82 @@
 package Exeption_Handling_JM;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 class Hello2 {
-	void display() throws FileNotFoundException, IOException, Exception {
-		FileReader fr = new FileReader("../java_practicle/src/Exeption_Handling_JM/Rohan.txt");
-		BufferedReader br = new BufferedReader(fr);
-		// System.out.println(br.readLine()); //print single line from file
 
-		String content;
+    // Read data from file
+    void display() throws FileNotFoundException, IOException, Exception {
 
-		while ((content = br.readLine()) != null) {
+        FileReader fr = new FileReader("../java_practicle/src/Exeption_Handling_JM/Rohan.txt");
+        BufferedReader br = new BufferedReader(fr);
 
-			System.out.println(content);
+        String content;
 
-		}
-		br.close();
-	}
+        System.out.println("----- File Content -----");
+
+        while ((content = br.readLine()) != null) {
+            System.out.println(content);
+        }
+
+        br.close();
+    }
+
+    // Write data into file
+    void writecode() throws IOException {
+
+        File f1 = new File("../java_practicle/src/Exeption_Handling_JM/Rohan.txt");
+
+        // true = append mode
+        FileWriter fw = new FileWriter(f1, true);
+
+        String data = "\nHello Students";
+
+        fw.write(data);
+        fw.append('\n');
+        fw.append('J');
+
+        fw.close();
+
+        System.out.println("\nData Written Successfully...");
+    }
 }
 
 public class Throws_ex {
 
-	public static void main(String[] args) {
-		Hello2 ob = new Hello2();
-		try {
-			ob.display();
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (IOException e1) {
-			System.out.println(e1);
-		} catch (Exception e2) {
-			System.out.println(e2);
-		}
+    public static void main(String[] args) {
 
-	}
+        Hello2 ob = new Hello2();
 
+        try {
+
+            // Read existing file
+            ob.display();
+
+            // Write new data
+            ob.writecode();
+
+            // Read file again after writing
+            System.out.println("\n----- Updated File Content -----");
+            ob.display();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("File Not Found: " + e);
+
+        } catch (IOException e) {
+
+            System.out.println("IOException: " + e);
+
+        } catch (Exception e) {
+
+            System.out.println("Exception: " + e);
+
+        }
+
+    }
 }
