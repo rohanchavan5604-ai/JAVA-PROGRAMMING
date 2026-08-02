487345a537ec8eed4161404cd8f156122b75c481
Rohan Chavan <rohanchavan5604@gmail.com>
2026-08-02 20:49:12 +0530

    List Example

---
+++ java_practicle/src/collection_JM/ListExample.java
@@ -0,0 +1,18 @@
+package collection_JM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<>();
+
+		list.add("Java");
+		list.add("Python");
+		list.add("SQL");
+
+		System.out.println(list);
+	}
+}
